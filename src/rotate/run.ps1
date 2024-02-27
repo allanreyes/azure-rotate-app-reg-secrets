05aa92b8109f35c4985ef8a952bec0fa6dca109c
Allan Reyes <allanreyes@microsoft.com>
2024-02-27 14:51:54 -0500

    implement

--- src/rotate/run.ps1
+++ src/rotate/run.ps1
@@ -1,25 +1,54 @@
 using namespace System.Net
 
-# Input bindings are passed in via param block.
-param($Request, $TriggerMetadata)
+   # Connect-MgGraph -Identity -TenantId  -Scopes "User.Read","Application.ReadWrite.All"
+   # Connect-AzAccount  -TenantId 
 
-# Write to the Azure Functions log stream.
-Write-Host "PowerShell HTTP trigger function processed a request."
+param($Request, $AppEntity, $TriggerMetadata)
 
-# Interact with query parameters or the body of the request.
-$name = $Request.Query.Name
-if (-not $name) {
-    $name = $Request.Body.Name
+$vaultName = $env:KeyVaultName
+
+# Login to Graph using same context
+$token = Get-AzAccessToken -ResourceUri 'https://graph.microsoft.com/'
+Write-Host "Acwuired a token for $($token.UserId)"
+Connect-MgGraph -AccessToken (ConvertTo-SecureString $token.Token -AsPlainText -Force)
+
+# Get all app registrations in scope
+foreach($entity in $AppEntity)
+{
+    $appId = $entity.RowKey
+    $apps = Get-MgApplication -Filter "AppId  eq '$appId'" -Property AppId, DisplayName, PasswordCredentials, Id 
+    
+    if($apps){
+        $app = $apps | Select-Object -First 1
+        $objectId = $app.Id
+
+        $params = @{
+            passwordCredential = @{
+                displayName = "Password created on $($(Get-Date -Format 'yyyy-MM-dd'))"
+                endDateTime = (Get-Date).AddDays(5)
+            }
+        }
+        $secret = Add-MgApplicationPassword -ApplicationId $objectId -BodyParameter $params
+
+        # Convert new secret value to SecureString
+        $secretvalue = ConvertTo-SecureString $secret.SecretText -AsPlainText -Force
+
+        # Upsert secret in keyvault
+        Set-AzKeyVaultSecret -VaultName $vaultName -Name $appId -SecretValue $secretvalue
+
+        # Delete Expired Secrets in App Registrations in scope
+        foreach($key in $app.PasswordCredentials | Where-Object { $_.endDateTime -lt (Get-Date) } ){
+            $params = @{
+                keyId = $key.keyId
+            }
+            Remove-MgApplicationPassword -ApplicationId $objectId -BodyParameter $params
+        }
+
+} else {
+    Write-Error "Application with client id $appId not found"
+}
 }
 
-$body = "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-
-if ($name) {
-    $body = "Hello, $name. This HTTP triggered function executed successfully."
-}
-
-# Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
     StatusCode = [HttpStatusCode]::OK
-    Body = $body
 })
